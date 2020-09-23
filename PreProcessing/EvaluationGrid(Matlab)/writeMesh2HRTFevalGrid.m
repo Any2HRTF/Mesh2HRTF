a328c77149f712d1a595c3e095e7cc305f0dc9a3
f-brinkmann <fabian.brinkmann@mailbox.org>
2020-09-23 13:55:52 +0200

    Update writeMesh2HRTFevalGrid.m
    Added an additional input parameter to make it possible to use multiple custom evaluation grids in a single simulation. See documentation inside WriteMesh2HRTFevaluationGrid.m for details.
    
    original commit  fbrinkmann 2020-03-20

--- PreProcessing/EvaluationGrid(Matlab)/writeMesh2HRTFevalGrid.m
+++ PreProcessing/EvaluationGrid(Matlab)/writeMesh2HRTFevalGrid.m
@@ -1,12 +1,23 @@
 % D O C U M E N T A T I O N
-% function WriteMesh2HRTFevalGrid(nodes, path, doPlot)
+% function WriteMesh2HRTFevalGrid(nodes, path, doPlot, startCount)
 %
 % writes evaluation grid in the format required by mesh2HRTF in the
-% location specified by path. nodes specify the spatial sampling points by
-% x, y, and z coordinates in meter. nodes of dimension Qx3, where Q are the
-% number of sampling points.
-% doPlot (true, false) specifies weather or not to plot the grid passed to
-% the function.
+% location specified by path.
+%
+% nodes      - a [Q x 3] matrix that specifies the x/y/z coordinates of the
+%              evaluation grid in meter. Q is the number of sampling points
+% path       - string that specifies the path where the data are saved.
+% doPlot     - boolean that specifies weather or not to plot the sampling
+%              grid (default = true)
+% startCount - Mesh2HRTF saves the sampling nodes in Nodes.txt. This file
+%              assigns an integer ID to each node. startCount specifies the
+%              ID of the first node. The second note will hav the ID
+%              startCount+1, etc (default = 200000).
+%
+%              IMPORTANT: If you use more than one evaluation grid in a
+%              single simulation, each node in each grid must have a unique
+%              ID. For example use a startCount of 200000 for the first
+%              grid and a startCount of 300000 for the second.
 %
 % Author: fabian.brinkmann@tu-berlin.de
 % Audio Communication Group @ TU Berlin
@@ -26,16 +37,22 @@
 % - In your publication, cite both articles:
 %   [1] Ziegelwanger, H., Kreuzer, W., and Majdak, P. (2015). "Mesh2HRTF: Open-source software package for the numerical calculation of head-related transfer functions," in Proceedings of the 22nd ICSV, Florence, IT.
 %   [2] Ziegelwanger, H., Majdak, P., and Kreuzer, W. (2015). "Numerical calculation of listener-specific head-related transfer functions and sound localization: Microphone model and mesh discretization," The Journal of the Acoustical Society of America, 138, 208-222.
-function WriteMesh2HRTFevalGrid(nodes, path, doPlot)
+function WriteMesh2HRTFevalGrid(nodes, path, doPlot, startCount)
+
+if ~exist('doPlot', 'var')
+    doPlot = true;
+end
+
+% start count for numbering the nodes, and elements in Nodes.txt, and
+% Elements.txt
+if ~exist('startCount', 'var')
+    startCount = 200000;
+end
 
 % triangulate the nodes
 elements  = delaunayTriangulation(nodes(:,1), nodes(:,2), nodes(:,3)); 
 % get only the surface of the triangulation
 elements  = freeBoundary(elements);
-
-% start count for numbering the nodes, and elements in Nodes.txt, and
-% Elements.txt
-start_count = 200000;
 
 % check and make the path
 if ~exist(path, 'dir')
@@ -47,7 +64,7 @@ fileID = fopen(fullfile(path, 'Nodes.txt'), 'w+');
 fprintf(fileID, '%u\n', size(nodes,1));
 
 for n = 1:size(nodes,1)
-    fprintf(fileID, '%u %.6f %.6f %.6f\n', start_count+n-1, nodes(n,1), nodes(n,2), nodes(n,3));
+    fprintf(fileID, '%u %.6f %.6f %.6f\n', startCount+n-1, nodes(n,1), nodes(n,2), nodes(n,3));
 end
 
 fclose(fileID);
@@ -58,10 +75,10 @@ fprintf(fileID, '%u\n', size(elements,1));
 
 for n = 1:size(elements,1)
     fprintf(fileID, '%u %u %u %u %u %u %u\n', ...
-            start_count+n-1, ...
-            elements(n,1)+start_count-1, ...
-            elements(n,2)+start_count-1, ...
-            elements(n,3)+start_count-1, ...
+            startCount+n-1, ...
+            elements(n,1)+startCount-1, ...
+            elements(n,2)+startCount-1, ...
+            elements(n,3)+startCount-1, ...
             2, 0, 1);
 end
 
