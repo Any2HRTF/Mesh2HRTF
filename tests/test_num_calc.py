eb08f10340b5de6ff17f7f0cd934da798cda8a41
Jeffrey Thomsen <j.thomsen@campus.tu-berlin.de>
2021-03-30 17:46:36 +0200

    Make test for NumCalc make

--- tests/test_num_calc.py
+++ tests/test_num_calc.py
@@ -1,6 +1,15 @@
 import subprocess
 import tempfile
+import shutil
+import os
+
+# create a temporary directory
+tmp = tempfile.TemporaryDirectory(dir=os.getcwd())
 
 
 def test_build():
-    assert 3 == 3
+    """ test if make for NumCalc works """
+
+    shutil.copytree("../mesh2hrtf/NumCalc/Source", tmp.name+"/NumCalc")
+    os.chdir(tmp.name+"/NumCalc")
+    subprocess.run(["make"], check=True)
