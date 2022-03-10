a8e7238d07da968311a503a84dcb4c58425558ae
Jeffrey Thomsen <j.thomsen@campus.tu-berlin.de>
2022-03-10 18:28:39 +0100

    Fix and clean up numcalc command line parameter tests

--- tests/test_num_calc.py
+++ tests/test_num_calc.py
@@ -29,11 +29,55 @@ def test_build_numcalc():
 
     # Verify - missing
 
-@pytest.mark.parametrize("nitermax, istart, iend", [(1, 0, 0), (2, 0, 0),
-                                                    (0, 3, 0), (0, 0, 3),
-                                                    (0, 2, 3), (2, 2, 3)])
-def test_numcalc_commandline_parameters(nitermax, istart, iend):
-    """ test if NumCalc's command line parameters behave as expected"""
+@pytest.mark.parametrize("nitermax, use", [(0, True), (1, True), (2, True), 
+                                             ([], False)])
+def test_numcalc_commandline_nitermax(nitermax, use):
+    """ test if NumCalc's command line parameter nitermax behaves as expected"""
+    # Setup
+
+    # create temporary directory
+    tmp = tempfile.TemporaryDirectory(dir=os.getcwd())
+
+    # copy test directory
+    shutil.copytree(os.path.join(os.path.dirname(__file__),
+                                 'test_numcalc_project'),
+                    os.path.join(tmp.name, 'project'))
+    # copy correct input file and rename it to NC.inp
+    shutil.copyfile(os.path.join(os.path.dirname(__file__),
+                    'test_numcalc_input_files', 'NC_commandline_parameters.inp'),
+                    os.path.join(tmp.name, 'project', 'NumCalc',
+                                 'source_1', 'NC.inp'))                                
+    
+    if use:
+        commandLineArgument = f' -nitermax {nitermax}'
+    else:
+        commandLineArgument = ''
+
+    # Exercise
+
+    # run NumCalc with subprocess
+    tmp_path = os.path.join(tmp.name, "project", "NumCalc", "source_1")
+    subprocess.run([f'NumCalc{commandLineArgument}'], cwd=tmp_path, check=True,
+                   shell=True)
+
+    # Verify
+    out_filename = 'NC.out'
+    out_filepath = os.path.join(tmp.name, "project", "NumCalc",
+                                "source_1", out_filename)
+
+    out_file = open(out_filepath)
+    out_text = out_file.read()
+
+    if use:
+        assert f'CGS solver: number of iterations = {nitermax}'    in out_text
+        assert 'Warning: Maximum number of iterations is reached!' in out_text
+    else:
+        assert 'Warning: Maximum number of iterations is reached!' not in out_text
+
+@pytest.mark.parametrize("istart, iend", [(False, False), (3, False),
+                                          (False, 3), (2, 3)])
+def test_numcalc_commandline_istart_iend(istart, iend):
+    """ test if NumCalc's command line parameters istart and iend behave as expected"""
     # Setup
 
     # create temporary directory
@@ -50,8 +94,6 @@ def test_numcalc_commandline_parameters(nitermax, istart, iend):
                                  'source_1', 'NC.inp'))                                
     
     commandLineArgument = ''
-    if nitermax > 0:
-        commandLineArgument += f' -nitermax {nitermax}'
     if istart > 0:
         commandLineArgument += f' -istart {istart}'
     if iend > 0:
@@ -65,11 +107,11 @@ def test_numcalc_commandline_parameters(nitermax, istart, iend):
                    shell=True)
 
     # Verify
-    if ((istart == 0) and (iend == 0)):
+    if (not istart and not iend):
         out_filename = 'NC.out'
-    elif ((istart > 0) and (iend == 0)):
+    elif ((istart > 0) and not iend):
         out_filename = f'NCfrom{istart}.out'
-    elif ((istart == 0) and (iend > 0)):
+    elif (not istart and (iend > 0)):
         out_filename = f'NCuntil{iend}.out'
     elif ((istart > 0) and (iend > 0)):
         out_filename = f'NC{istart}-{iend}.out'
@@ -79,31 +121,23 @@ def test_numcalc_commandline_parameters(nitermax, istart, iend):
     out_filepath = os.path.join(tmp.name, "project", "NumCalc",
                                 "source_1", out_filename)
 
-    reference_filepath = os.path.join(os.path.dirname(__file__),
-                 'test_numcalc_commandline_parameters_reference_files',
-                 f'NC_{nitermax}_{istart}_{iend}.out')
+    out_file = open(out_filepath)
+    out_text = out_file.read()
 
-    with open(out_filepath) as f:
-        out_lines = f.readlines()
+    if istart > 0:
+        assert f'Step {istart-1}' not in out_text
+        assert f'Step {istart}'       in out_text
+    else:
+        assert 'Step 1' in out_text
 
-    with open(reference_filepath) as f:
-        ref_lines = f.readlines()
+    if iend > 0:
+        assert f'Step {iend}'       in out_text
+        assert f'Step {iend+1}' not in out_text
 
-    istep = 0 # to count correct number of istart iend steps
-    for number, line in enumerate(out_filepath):
-        if line.startswith('Warning'): # check nitermax = 1 condition
-            assert(line == ref_lines[number])
-        if line.startswith('CGS solver'): # check arbitrary nitermax condition
-            assert(line == ref_lines[number])
-        if ((istart > 0) or (iend > 0)): # check istart iend conditions
-            if line.startswith('>> S T E P   N U M B E R   A N D   F R E Q U E N C Y <<'):
-                assert(out_lines[number+2] == ref_lines[number+2])
-
-                if (iend > 0) and (istart+istep > iend):
-                    raise Exception("Step number exceeds iend value")
-                else:
-                    assert(out_lines[number+2].startswith(f'Step {istart+istep}'))
-                    istep += 1
+    if istart>0 and iend>0:
+        nStepsActual = out_text.count('>> S T E P   N U M B E R   A N D   F R E Q U E N C Y <<')
+        nStepsExpected = iend - istart + 1
+        assert nStepsActual == nStepsExpected
 
 
 @pytest.mark.parametrize("boundary_condition", [("rigid"), ("soft")])
@@ -254,3 +288,7 @@ def test_numcalc_ear_source_types(boundary_condition, source, bem_method,
 
 # test_numcalc_ear_source_types('rigid', 'leftear', 'ml-fmm-bem',
 #                                   (40, -40), range_b=(-1, 1))
+# nitermax = 1
+# istart = False
+# iend= False
+# test_numcalc_commandline_parameters(nitermax, istart, iend)
