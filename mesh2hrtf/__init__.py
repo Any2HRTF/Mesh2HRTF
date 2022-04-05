6717dc27d6653288bfa9cb641b1bbaaec6b79541
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2022-04-05 11:44:03 +0200

    Update __init__.py

--- mesh2hrtf/__init__.py
+++ mesh2hrtf/__init__.py
@@ -2,10 +2,10 @@ from .Output2HRTF.Python.Output2HRTF_Main import (
     Output2HRTF_Main,
     reference_HRTF,
     compute_HRIR,
-    join_sofa_files)
+    merge_sofa_files)
 
 __all__ = [
     'Output2HRTF_Main',
     'reference_HRTF',
     'compute_HRIR',
-    'join_sofa_files']
+    'merge_sofa_files']
