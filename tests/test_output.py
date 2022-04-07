f7e314c55d943a3e09e5526922f275493bac396d
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2022-04-07 14:36:43 +0200

    also test projects with one source

--- tests/test_output.py
+++ tests/test_output.py
@@ -1,4 +1,5 @@
 # %%
+import pytest
 import subprocess
 from tempfile import TemporaryDirectory
 import shutil
@@ -9,10 +10,15 @@ import sofar as sf
 data_shtf = os.path.join(
     os.path.dirname(__file__), 'resources', 'SHTF')
 
-
-def test_Output2HRTF_Main_and_Output2HRTF():
+@pytest.mark.parametrize("num_sources", ([1], [2]))
+def test_Output2HRTF_Main_and_Output2HRTF(num_sources):
     """
-    Run Output2HRTF.py script to do a round trip test
+    Run Output2HRTF.py script to do a round trip test:
+
+    - does Output2HRTF_Main.py run without errors for projects with 1 and 2
+      sources
+    - are the report_source_*.csv files written correctly (2 sources only)
+    - are the SOFA files written correctly (2 sources only)
     """
 
     # copy test data to new directory and delete output data
@@ -21,8 +27,22 @@ def test_Output2HRTF_Main_and_Output2HRTF():
     shutil.copytree(data_shtf, tmp_shtf)
     shutil.rmtree(os.path.join(tmp_shtf, "Output2HRTF"))
 
+    # manipulate output script
+    if num_sources == 1:
+        with open(os.path.join(tmp_shtf, "Output2HRTF.py"), "r") as f:
+            script = "".join(f.readlines())
+
+        script.replace("num_sources = 2", "num_sources = 1")
+        assert "num_sources = 1" in script
+
+        with open(os.path.join(tmp_shtf, "Output2HRTF.py"), "w") as f:
+            f.write(script)
+
     # run Output2HRTF.py
     subprocess.run(["python", "Output2HRTF.py"], cwd=tmp_shtf, check=True)
+
+    if num_sources == 1:
+        return
 
     # compare reports
     reports = ["report_source_1.csv", "report_source_2.csv"]
