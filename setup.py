72e13f28911c47b87d7092baf07356ceb5a40b8f
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2022-04-12 09:02:58 +0200

    update paths in setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@
 
 from setuptools import setup, find_packages
 
-with open('readme.md') as readme_file:
+with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('history.txt') as history_file:
+with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 with open('VERSION') as version_file:
