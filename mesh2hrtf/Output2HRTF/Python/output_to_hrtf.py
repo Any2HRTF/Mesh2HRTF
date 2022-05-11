a5ca7eb959056c2d7b634da23aad4e3e5a485758
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2022-05-11 07:48:13 +0200

    add user defined sampling rates (subject to testing)

--- mesh2hrtf/Output2HRTF/Python/output_to_hrtf.py
+++ mesh2hrtf/Output2HRTF/Python/output_to_hrtf.py
@@ -164,8 +164,8 @@ def output_to_hrtf(
                 raise ValueError("Computing HRIRs requires prior referencing")
 
             # calculate shift value (equivalent to a 30 cm shift)
-            fs = round(2*sofa.N[-1])
-            n_shift = int(np.round(.30 / (1/fs * speedOfSound)))
+            sampling_rate = round(2*sofa.N[-1])
+            n_shift = int(np.round(.30 / (1/sampling_rate * speedOfSound)))
 
             sofa = compute_hrir(sofa, n_shift)
             sf.write_sofa(os.path.join(
@@ -279,7 +279,7 @@ def reference_hrtf(sofa, sourceType, sourceArea, speedOfSound, densityOfAir,
     return sofa
 
 
-def compute_hrir(sofa, n_shift):
+def compute_hrir(sofa, n_shift, sampling_rate=None):
     """
     Compute HRIR from HRTF by means of the inverse Fourier transform.
 
@@ -311,6 +311,11 @@ def compute_hrir(sofa, n_shift):
        SimpleFreeFieldHRTF or GeneralTF
     n_shift : int
         Amount the HRIRs are shifted to enforce a causal system.
+    sampling_rate : int
+        The sampling rate in Hz. The sampling rate can two times any frequency
+        for which the HRTF was computed. The default ``None`` assumes the
+        sampling rate to be two times the highest frequency for which the HRTF
+        was computed.
 
     Returns
     -------
@@ -339,14 +344,30 @@ def compute_hrir(sofa, n_shift):
             ('The frequency vector must go from f_1 > 0 to'
              'f_2 (half the sampling rate) in equidistant steps.'))
 
+    # get the HRTF
     pressure = sofa.Data_Real + 1j * sofa.Data_Imag
 
-    fs = round(2*frequencies[-1])
+    if sampling_rate is None:
+        # detect the sampling rate
+        sampling_rate = round(2*frequencies[-1])
+    else:
+        # check if the specified sampling rate is valid
+        idx = np.argmin(np.abs(2 * frequencies - sampling_rate))
+        error = np.abs(2 * frequencies[idx] - sampling_rate)
+        if np.abs(error) > 1e-6:
+            raise ValueError((
+                "The specified sampling rate is invalid. It must be two times "
+                "any frequency for which the HRTF was computed."))
+
+        # discard frequencies above sampling_rate / 2
+        pressure = pressure[..., :idx + 1]
+        frequencies = frequencies[:idx + 1]
 
     # add 0 Hz bin
     pressure = np.concatenate((np.ones((pressure.shape[0],
                                pressure.shape[1], 1)), pressure), axis=2)
-    # make fs/2 real
+
+    # make sampling_rate/2 real
     pressure[:, :, -1] = np.abs(pressure[:, :, -1])
     # ifft (take complex conjugate because sign conventions differ)
     hrir = np.fft.irfft(np.conj(pressure))
@@ -359,7 +380,7 @@ def compute_hrir(sofa, n_shift):
     sofa = _get_sofa_object(
         hrir, sofa.SourcePosition, sofa.SourcePosition_Type,
         sofa.ReceiverPosition, "HRIR", sofa.GLOBAL_ApplicationVersion,
-        sampling_rate=fs)
+        sampling_rate=sampling_rate)
 
     return sofa
 
