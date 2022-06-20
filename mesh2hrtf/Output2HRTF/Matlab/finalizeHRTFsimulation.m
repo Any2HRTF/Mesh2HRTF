76da17773f93cbe6bb079e74ff1f647c28b0e17f
Katharina Pollack <katharina@fritzis.at>
2022-06-20 21:56:28 +0200

    replace DONE fprintf command with disp command

--- mesh2hrtf/Output2HRTF/Matlab/finalizeHRTFsimulation.m
+++ mesh2hrtf/Output2HRTF/Matlab/finalizeHRTFsimulation.m
@@ -307,7 +307,7 @@ if isfile(fullfile(folder_names{1}, 'Info.txt'))
     copyfile(fullfile(folder_names{1}, 'Info.txt'), targetdir);
 end
 
-fprintf(['\nDone! Merged SOFA files saved in ', targetdir, '\n']);
+disp(['Done! Merged SOFA files saved in ', targetdir]);
 
 end
 % EOF
