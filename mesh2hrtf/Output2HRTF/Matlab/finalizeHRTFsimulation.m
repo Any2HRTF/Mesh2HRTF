d363d2ce2f600e9f05c15ee6e1f15006e5e25758
Katharina Pollack <katharina.pollack@oeaw.ac.at>
2022-06-21 16:27:09 +0200

    include targetdir as input and check whether it's pwd

--- mesh2hrtf/Output2HRTF/Matlab/finalizeHRTFsimulation.m
+++ mesh2hrtf/Output2HRTF/Matlab/finalizeHRTFsimulation.m
@@ -23,10 +23,10 @@ function finalizeHRTFsimulation(varargin)
 %
 % mode C - 2 inputs = Merge the 2 projects that were given as input_1 and input_2.
 %   Merged SOFA file will be written in pwd.
-% 
+%
 % mode D - 3 inputs = merge 2 projects and save to folder in input 3
 %   Merged SOFA file will be written to input 3
-% 
+%
 % migrated from Python API from Sergejs Dombrovskis
 % author(s): Katharina Pollack, June 2022
 
@@ -55,7 +55,7 @@ switch numel(varargin)
             error('Input parameter has to be valid folder name.');
         end
         start_folder = varargin{1};
-        
+
         % pretty sure this can be solved in a more elegant way (than
         % copypasting the code from above ...)
         for ii = 3:numel(start_folder)
@@ -78,9 +78,9 @@ switch numel(varargin)
         folder_names{1} = varargin{1};
         folder_names{2} = varargin{2};
     case 3 % mode '3 inputs' = Merge 2 projects and safe merged SOFA file to input 3
-        if ~ischar(varargin{1}) || ~ischar(varargin{2}) || ischar(varargin{3})
+        if ~ischar(varargin{1}) || ~ischar(varargin{2}) || ~ischar(varargin{3})
             error('Allowed input parameter is string.');
-        elseif ~isfolder(varargin{1}) || ~isfolder(varargin{2}) ||~isfolder(varargin{3})
+        elseif ~isfolder(varargin{1}) || ~isfolder(varargin{2}) || ~isfolder(varargin{3})
             error('All input parameters have to be valid folder names.');
         end
         folder_names{1} = varargin{1};
@@ -99,6 +99,9 @@ clear ii
 if isempty(targetdir)
     if strcmp(folder_names{1}(end-1:end), '_L') || strcmp(folder_names{1}(end-1:end), '_R')
         targetdir = strcat(folder_names{1}(1:end-2), '_merged');
+    elseif strcmpi(folder_names{1}, pwdirectory) || strcmpi(folder_names{2}, pwdirectory)
+        error(['Please specify a target directory for the merged SOFA file to be stored as a third input. ', ...
+            'It should not be one of the input directories.']);
     else
         targetdir = pwdirectory;
     end
@@ -106,7 +109,8 @@ end
 
 % find any broken data in this project
 for ii = 1:2
-    fprintf(['Checking for errors in ', folder_names{ii}, '...\n']);
+    NCout_files = {};
+    disp(['Checking for errors in ', folder_names{ii}, ' ...']);
     cd(fullfile(folder_names{ii}, 'NumCalc'))
     % check for number of sources
     NCdir_tmp = dir;
@@ -115,7 +119,7 @@ for ii = 1:2
             'This script only merges SOFA files of two separately calculated sources.\n', ...
             'See documentation for details.'], folder_names{ii});
     end
-    cd(NCdir_tmp{3}) % ignoring '.' and '..' entries
+    cd(NCdir_tmp(3).name) % ignoring '.' and '..' entries
     % check for NC*.out files
     sourcedir_tmp = dir('NC*.out');
     for jj = 1:numel(sourcedir_tmp)
@@ -133,50 +137,52 @@ for ii = 1:2
         counter = 0; happy_end = 0; corrupt_freq = 0; freq = [];
         while ~feof(fid)
             counter = counter+1;
-            line=fgets(fid);
-            switch line
-                case frequency_key
-                    line_nr = counter; % save line number as well
-                    freq = [freq; line(length(frequency_key)+1:end-3)]; % omit last 3 chars (' Hz')
-                case non_convergence_key
-                    if freq(end) > 24e3 % affects very high frequencies only
-                        warning(['Non-Convergence detected at over 24kHz  (at ', num2str(freq(end)), ' Hz']);
-                    else
-                        warning(['PROBLEM!! - Non-Convergence issue in important range at ', num2str(freq(end)), ' Hz']);
-                        corrupt_freq = [corrupt_freq; freq(end)]; % note which at frequency the calculation failed
-                    end
-                case gaussean_lim_key
-                    if freq(end) > 24e3 % affects very high frequencies only
-                        warning(['Gaussean points limit issue detected at over 24kHz  (at ', num2str(freq(end)), ' Hz']);
-                    else
-                        warning(['PROBLEM!! - Gaussean points limit issue in important range at ', num2str(freq(end)), ' Hz']);
-                        corrupt_freq = [corrupt_freq; freq(end)]; % note which at frequency the calculation failed
-                    end
-                case happy_end_key
-                    happy_end = 1;
-                otherwise
+            line=fgetl(fid); % read line without newline character
+            if contains(line, frequency_key)
+                line_nr = counter; % save line number as well
+                startIdx=regexp(line, frequency_key);
+                freq = [freq; str2double(line(startIdx+length(frequency_key):end-3))]; % omit last 3 chars (' Hz')
+            end
+            if contains(line, non_convergence_key)
+                if freq(end) > 24e3 % affects very high frequencies only
+                    warning(['Non-Convergence detected at over 24kHz  (at ', num2str(freq(end)), ' Hz']);
+                else
+                    warning(['PROBLEM!! - Non-Convergence issue in important range at ', num2str(freq(end)), ' Hz']);
+                    corrupt_freq = [corrupt_freq; freq(end)]; % note which at frequency the calculation failed
+                end
+            end
+            if contains(line, gaussean_lim_key)
+                if freq(end) > 24e3 % affects very high frequencies only
+                    warning(['Gaussean points limit issue detected at over 24kHz  (at ', num2str(freq(end)), ' Hz']);
+                else
+                    warning(['PROBLEM!! - Gaussean points limit issue in important range at ', num2str(freq(end)), ' Hz']);
+                    corrupt_freq = [corrupt_freq; freq(end)]; % note which at frequency the calculation failed
+                end
+            end
+            if contains(line, happy_end_key)
+                happy_end = 1;
             end
         end
-        fclose(fid);
-        if happy_end == 1
-            disp(['Great! The calculation of', NCout_files{kk}, 'finished successfully.']);
-        else
-            disp('FYI: The calculation of', NCout_files{kk}, 'did not finish successfully.');
-        end
-        
-        if numel(corrupt_freq) > 1 % we have corrupt data
-            if lowest_corrupt_frq(ii) == 0
-                lowest_corrupt_frq(ii) = min(corrupt_freq);
-            elseif lowest_corrupt_frq(ii) > min(corrupt_freq)
-                lowest_corrupt_frq(ii) = min(corrupt_freq); % keep only the lowest problematic frequency
-            end
+    end
+    fclose(fid);
+    if happy_end == 1
+        warning(['Great! The calculation of', NCout_files{kk}, ' finished successfully.']);
+    else
+        warning(['FYI: The calculation of', NCout_files{kk}, ' did not finish successfully.']);
+    end
+
+    if numel(corrupt_freq) > 1 % we have corrupt data
+        if lowest_corrupt_frq(ii) == 0
+            lowest_corrupt_frq(ii) = min(corrupt_freq);
+        elseif lowest_corrupt_frq(ii) > min(corrupt_freq)
+            lowest_corrupt_frq(ii) = min(corrupt_freq); % keep only the lowest problematic frequency
         end
     end
 end
 clear ii
 
-if (lowest_corrupt_frq(1) > 1 || lowest_corrupt_frq(1) < 250) || ...
-        (lowest_corrupt_frq(2) > 1 || lowest_corrupt_frq(2) < 250)
+if (lowest_corrupt_frq(1) > 1 && lowest_corrupt_frq(1) < 250) || ...
+        (lowest_corrupt_frq(2) > 1 && lowest_corrupt_frq(2) < 250)
     error(['Practically all simulation instances failed. There is no usable data here. ', ...
         'See documentation for troubleshooting or open an issue in the repository.']);
 end
@@ -191,20 +197,31 @@ end
 for ii = 1:2
     % run Output2HRTF script in project folder
     cd(folder_names{ii})
-    fprintf(['Running Output2HRTF in folder ', folder_names{ii}, '...\n']);
-    Output2HRTF;
+    if isfolder(fullfile(folder_names{ii}, 'Output2HRTF')) && ~isempty(dir(fullfile(folder_names{ii}, 'Output2HRTF', '*.sofa')))
+        warning(['There are already SOFA files in ', fullfile(folder_names{ii}, 'Output2HRTF'), ...
+            '. If you want to overwrite them, please delete the .sofa files and rerun finalizeHRTFsimulation.m']);
+    else
+        disp(['Running Output2HRTF in folder ', folder_names{ii}, ' ...']);
+        Output2HRTF;
+    end
     % determine left and right ear data
     % source position along interaural axis. Positive = left, negative = right
-    fprintf(['Determine L/R ear of project ', folder_names{ii}, '...\n']);
+    disp(['Determine L/R ear of project ', folder_names{ii}, ' ...']);
     output2hrtf_text = fileread('Output2HRTF.m');
     [~, endIdx] = regexp(output2hrtf_text, '(sourceType = )');
     switch output2hrtf_text(endIdx+2)
         case 'L'
             sofa_file_L = folder_names{ii};
-            sofa_files_L = dir('*.sofa');
+            sofa_files_tmp = dir(fullfile(folder_names{ii}, 'Output2HRTF', '*.sofa'));
+            for jj = 1:numel(sofa_files_tmp)
+                sofa_files_L{jj} = fullfile(sofa_files_tmp(jj).folder, sofa_files_tmp(jj).name);
+            end
         case 'R'
             sofa_file_R = folder_names{ii};
-            sofa_files_R = dir('*.sofa');
+            sofa_files_tmp = dir(fullfile(folder_names{ii}, 'Output2HRTF', '*.sofa'));
+            for jj = 1:numel(sofa_files_tmp)
+                sofa_files_R{jj} = fullfile(sofa_files_tmp(jj).folder, sofa_files_tmp(jj).name);
+            end
     end
 end
 clear ii
@@ -212,11 +229,11 @@ clear ii
 % merge to one SOFA file
 % merge_write_sofa(sofa_left, sofa_right, basepath, filename, sofa_type='HRTF'):
 % merge_write_sofa(sofa_left, sofa_right, basepath, filename, sofa_type='HRIR'):
-fprintf('Merge SOFA file(s) ...\n');
+disp('Merge SOFA file(s) ...');
 for ii = 1:numel(sofa_files_L)  % loop for every SOFA file in one of the projects
-    sofa_read_L = SOFAload(sofa_files_L(ii).name);
-    sofa_read_R = SOFAload(sofa_files_R(ii).name);
-    
+    sofa_read_L = SOFAload(sofa_files_L{ii});
+    sofa_read_R = SOFAload(sofa_files_R{ii});
+
     % detect data type
     if strcmp(sofa_read_L.GLOBAL_DataType, 'FIR')
         SOFA_type = 'HRIR';
@@ -231,76 +248,76 @@ for ii = 1:numel(sofa_files_L)  % loop for every SOFA file in one of the project
     else
         SOFA_type = 'HRTF';
         % sanity check whether files can be merged
-        if sofa_read_L.N - sofa_read_R.N < 1e-10
+        if unique((sofa_read_L.N - sofa_read_R.N) > 1e-10)
             error('Frequency vectors of the two input SOFA files do not match! Merging aborted.');
         end
     end
+
+    % write merged SOFA file to targetdir
+    fprintf(['Write merged SOFA file to ...', targetdir,'\n']);
+    % create targetdir if it does not exist yet
+    if ~isfolder(targetdir)
+        mkdir(targetdir)
+    end
+    cd(targetdir);
+
+    % create empty SOFA object
+    if strcmp(SOFA_type, 'HRTF')
+        Obj = SOFAgetConventions('SimpleFreeFieldHRTF');
+        % write data to SOFA object
+        Obj.Data.Real = [sofa_read_L.Data.Real; sofa_read_R.Data.Real];
+        Obj.Data.Imag = [sofa_read_L.Data.Imag; sofa_read_R.Data.Imag];
+        Obj.N = sofa_read_L.N;
+        fs_out = 2*Obj.N(end); % specify sampling frequency according to Nyquist
+    elseif strcmp(SOFA_type, 'HRIR')
+        Obj = SOFAgetConventions('SimpleFreeFieldHRIR');
+        % write data to SOFA object
+        Obj.Data = [sofa_read_L.Data.IR, sofa_read_R.Data.IR];
+        Obj.Data.SamplingRate = sofa_read_L.Data.SamplingRate;
+        Obj.Data.Delay = [sofa_read_L.Data.Delay, sofa_read_R.Data.Delay];
+        fs_out = Obj.Data_SamplingRate;
+    else
+        Obj = SOFAgetConventions('General'); % general SOFA object, no restrictions at all
+    end
+
+    % write meta data
+    Obj.GLOBAL_ApplicationName = sofa_read_L.GLOBAL_ApplicationName;
+    Obj.GLOBAL_ApplicationVersion = sofa_read_L.GLOBAL_ApplicationVersion;
+    Obj.GLOBAL_History = sofa_read_L.GLOBAL_History;
+
+    if isempty(sofa_read_L.GLOBAL_Title)
+        Obj.GLOBAL_Title = ['untitled ', SOFA_type, ' SOFA data'];
+    else
+        Obj.GLOBAL_Title = sofa_read_L.GLOBAL_Title;
+    end
+
+    if isempty(sofa_read_L.GLOBAL_Origin) % added for spat5
+        Obj.GLOBAL_Origin = 'Mesh2HRTF_simulation';
+    else
+        Obj.GLOBAL_Origin = sofa_read_L.GLOBAL_Origin;
+    end
+
+    % source and receiver data
+    if strcmp(sofa_read_L.SourcePosition_Type, 'cartesian') && strcmp(sofa_read_L.SourcePosition_Units, 'metre')
+        [pos_tmp(:,1), pos_tmp(:,2), pos_tmp(:,3)] = cart2sph(sofa_read_L.SourcePosition(1), sofa_read_L.SourcePosition(2), sofa_read_L.SourcePosition(3));
+        Obj.SourcePosition = rad2deg(pos_tmp);
+        Obj.SourcePosition_Units = 'degree, degree, metre';
+        Obj.SourcePosition_Type = 'spherical';
+        clear pos_tmp
+    else
+        Obj.SourcePosition = sofa_read_L.SourcePosition;
+        Obj.SourcePosition_Units = sofa_read_L.SourcePosition_Units;
+        Obj.SourcePosition_Type = sofa_read_L.SourcePosition_Type;
+    end
+
+    Obj.ReceiverPosition = [sofa_read_L.ReceiverPosition; sofa_read_R.ReceiverPosition];
+    Obj.ReceiverPosition_Units = sofa_read_L.ReceiverPosition_Units;
+    Obj.ReceiverPosition_Type = sofa_read_L.ReceiverPosition_Type;
+
+    % save as SOFA file
+    Obj=SOFAupdateDimensions(Obj);
+    SOFAsave(fullfile(targetdir, [SOFA_type, '_', num2str(fs_out), 'fs.sofa']), Obj);
 end
-
-% write merged SOFA file to targetdir
-fprintf(['Write merged SOFA file to ...', targetdir,'\n']);
-% create targetdir if it does not exist yet
-if ~isfolder(targetdir)
-    mkdir(targetdir)
-end
-cd(targetdir);
-
-% create empty SOFA object
-if strcmp(SOFA_type, 'HRTF')
-    Obj = SOFAgetConventions('SimpleFreeFieldHRTF');
-    % write data to SOFA object
-    Obj.Data.Real = [sofa_read_L.Data.Real; sofa_read_R.Data.Real];
-    Obj.Data.Imag = [sofa_read_L.Data.Imag; sofa_read_R.Data.Imag];
-    Obj.N = sofa_read_L.N;
-    fs_out = 2*Obj.N(end); % specify sampling frequency according to Nyquist
-elseif strcmp(SOFA_type, 'HRIR')
-    Obj = SOFAgetConventions('SimpleFreeFieldHRIR');
-    % write data to SOFA object
-    Obj.Data = [sofa_read_L.Data.IR, sofa_read_R.Data.IR];
-    Obj.Data.SamplingRate = sofa_read_L.Data.SamplingRate;
-    Obj.Data.Delay = [sofa_read_L.Data.Delay, sofa_read_R.Data.Delay];
-    fs_out = Obj.Data_SamplingRate;
-else
-    Obj = SOFAgetConventions('General'); % general SOFA object, no restrictions at all
-end
-
-% write meta data
-Obj.GLOBAL_ApplicationName = sofa_read_L.GLOBAL_ApplicationName;
-Obj.GLOBAL_ApplicationVersion = sofa_read_L.GLOBAL_ApplicationVersion;
-Obj.GLOBAL_History = sofa_read_L.GLOBAL_History;
-
-if isempty(sofa_read_L.GLOBAL_Title)
-    Obj.GLOBAL_Title = ['untitled ', SOFA_type, ' SOFA data'];
-else
-    Obj.GLOBAL_Title = sofa_read_L.GLOBAL_Title;
-end
-
-if isempty(sofa_read_L.GLOBAL_Origin) % added for spat5
-    Obj.GLOBAL_Origin = 'Mesh2HRTF_simulation';
-else
-    Obj.GLOBAL_Origin = sofa_read_L.GLOBAL_Origin;
-end
-
-% source and receiver data
-if strcmp(sofa_read_L.SourcePosition_Type, 'cartesian') && strcmp(sofa_read_L.SourcePosition_Units, 'metre')
-    [pos_tmp(:,1), pos_tmp(:,2), pos_tmp(:,3)] = cart2sph(sofa_read_L.SourcePosition(1), sofa_read_L.SourcePosition(2), sofa_read_L.SourcePosition(3));
-    Obj.SourcePosition = rad2deg(pos_tmp);
-    Obj.SourcePosition_Units = 'degree, degree, metre';
-    Obj.SourcePosition_Type = 'spherical';
-    clear pos_tmp
-else
-    Obj.SourcePosition = sofa_read_L.SourcePosition;
-    Obj.SourcePosition_Units = sofa_read_L.SourcePosition_Units;
-    Obj.SourcePosition_Type = sofa_read_L.SourcePosition_Type;
-end
-
-Obj.ReceiverPosition = [sofa_read_L.ReceiverPosition; sofa_read_R.ReceiverPosition];
-Obj.ReceiverPosition_Units = sofa_read_L.ReceiverPosition_Units;
-Obj.ReceiverPosition_Type = sofa_read_L.ReceiverPosition_Type;
-
-% save as SOFA file
-Obj=SOFAupdateDimensions(Obj);
-SOFAsave(fullfile(targetdir, [SOFA_type, '_', num2str(fs_out), 'fs.sofa']), Obj);
 
 % copy Info.txt to targetdir
 if isfile(fullfile(folder_names{1}, 'Info.txt'))
