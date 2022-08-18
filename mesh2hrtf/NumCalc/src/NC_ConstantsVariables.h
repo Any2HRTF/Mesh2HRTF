0e3607e2b602e0e294c911da486065fbe123f78e
schaastrommla <kreiza@debian>
2022-08-18 09:37:56 +0200

    pushed the limit for the name to 30 characters, if piotr wants to use alpha

--- mesh2hrtf/NumCalc/src/NC_ConstantsVariables.h
+++ mesh2hrtf/NumCalc/src/NC_ConstantsVariables.h
@@ -67,7 +67,7 @@ extern ofstream NCout;  /* output stream */
 extern FILE *inputFile_, *tmpFile_;   /* input and output files */
 
 extern char jobTitle_[SIZE_LINE],   /* title of the job */
-            versionNumber_[20];     /* version number */
+            versionNumber_[30];     /* version number */
 
 extern int currentFrequency_,   /* number of the current frequency */
 		   numFrequencies_;     /* number of frequencies */
