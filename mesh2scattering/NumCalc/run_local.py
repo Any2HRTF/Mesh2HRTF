171aa217e592fd1ba78186ae42069db1610fb8f5
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2023-01-27 00:44:45 +0100

    document workflow

--- mesh2scattering/NumCalc/run_local.py
+++ mesh2scattering/NumCalc/run_local.py
@@ -1,23 +1,24 @@
 # %%
-import mesh2hrtf as m2h
+import mesh2scattering as m2s
 from utils import folders
 import os
-project_name = '01_kunsthaus_zuerich'
-project_name = 'sine'
-data_path = folders.sciebo_data_path()
+
+# this is the project path which we want to simualte
+# it should contain a reference and a sample folder
+project_path = '/Users/anne/sciebo/2021_DFG-Projekt/data/mesh2scattering/sine_10k'
+
+# this is the path to the NumCalc executable, on Windows it will be a path like C:⁄NumCalc.exe 
+numcalc_path = 'NumCalc'
 
 # %%
 
-m2h.manage_numcalc(
-    os.path.join(
-        data_path, 'mesh2hrtf_results', project_name, 'reference'),
-    max_instances=8)
+m2s.manage_numcalc(
+    os.path.join(project_path, 'reference'),
+    numcalc_path)
 
-m2h.manage_numcalc(
-    os.path.join(
-        data_path, 'mesh2hrtf_results', project_name, 'sample'),
-    starting_order='high',
-    max_instances=8)
+m2s.manage_numcalc(
+    os.path.join(project_path, 'sample'),
+    numcalc_path)
 
 
 # %%
