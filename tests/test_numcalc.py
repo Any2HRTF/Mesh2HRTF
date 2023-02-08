c0482183c5a26815fca55d48bdb30c98ce6a5751
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2023-02-08 17:04:43 +0100

    fix unix tests

--- tests/test_numcalc.py
+++ tests/test_numcalc.py
@@ -24,10 +24,6 @@ else:
     numcalc = os.path.join(
         m2s.utils.repository_root(), "NumCalc", "bin", "NumCalc")
 
-    shutil.copytree(
-        os.path.join(base_dir, "..", "mesh2scattering", "NumCalc"),
-        os.path.join(m2s.utils.repository_root(), "NumCalc"))
-
     if os.path.isfile(numcalc):
         os.remove(numcalc)
 
@@ -252,10 +248,9 @@ def test_defaults(tmpdir):
         os.path.join(tmpdir, "SHTF", "NumCalc", "source_1", "be.out"))
     shutil.rmtree(os.path.join(tmpdir, "SHTF", "NumCalc", "source_2"))
 
-    numcalc_path = os.path.dirname(numcalc)
     # run as function
     m2s.NumCalc.manage_numcalc(
-        tmpdir, numcalc_path=numcalc_path, wait_time=0)
+        tmpdir, numcalc_path=numcalc, wait_time=0)
     # check if files exist
     assert len(glob.glob(os.path.join(tmpdir, "manage_numcalc_*txt")))
 
