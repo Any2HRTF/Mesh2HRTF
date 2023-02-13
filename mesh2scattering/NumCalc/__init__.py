c2665462edd6f8cc493c809f253353071a1d2725
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2023-02-13 12:45:50 +0100

    test

--- mesh2scattering/NumCalc/__init__.py
+++ mesh2scattering/NumCalc/__init__.py
@@ -1,12 +1,8 @@
 from .numcalc import (
-    manage_numcalc,
-    remove_outputs,
     read_ram_estimates,
     )
 
 
 __all__ = [
-    'manage_numcalc',
-    'remove_outputs',
     'read_ram_estimates',
     ]
